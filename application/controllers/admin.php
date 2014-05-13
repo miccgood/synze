e256cea174cb544640bf3b8b5b6a1bd20fb3aebd
PC <PC@PC-PC>
2014-05-13 23:54:05 +0700

    เพิ่ม Admin Page

---
+++ application/controllers/admin.php
@@ -0,0 +1,28 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Admin extends SpotOn {
+    function __construct() {
+        $this->autoSetDefaultValue = TRUE;
+        parent::__construct();
+    }
+    
+    function index() {
+        
+        $this->crud->set_table('mst_user')
+        ->set_subject('User')
+        ->display_as("user_displayname" , "Name")
+        ->set_relation_n_n('permission_ID', 'trn_permission', 'mst_permission', 'user_ID', 'permission_ID', 'page_name','seq')
+//        ->where("terminal.cpn_ID" , $this->cpnId)
+        ->columns('user_displayname', 'permission_ID')
+        ->fields('user_displayname', 'permission_ID')
+        ;
+        $this->output();
+        
+    }
+    
+    public function _beforeDelete($primary_key) {
+        return $this->m->checkDeleteTerminal($primary_key);
+    }
+    
+    
+}
